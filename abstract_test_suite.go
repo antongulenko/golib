7da03b65371f95e5667052d7d6838c858f64b374
Anton Gulenko <anton.gulenko@googlemail.com>
2020-03-23 14:44:18 +0100

    extended test suite lib

--- abstract_test_suite.go
+++ abstract_test_suite.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
 )
 
 // An example of how to use this type can be found in flags_test.go
@@ -13,11 +14,31 @@ type AbstractTestSuite struct {
 	*require.Assertions
 }
 
-func (suite *AbstractTestSuite) T() *testing.T {
-	return suite.t
+func (s *AbstractTestSuite) T() *testing.T {
+	return s.t
 }
 
-func (suite *AbstractTestSuite) SetT(t *testing.T) {
-	suite.t = t
-	suite.Assertions = require.New(t)
+func (s *AbstractTestSuite) SetT(t *testing.T) {
+	s.t = t
+	s.Assertions = require.New(t)
+}
+
+func (s *AbstractTestSuite) Run(t *testing.T) {
+	suite.Run(t, s)
+}
+
+func (s *AbstractTestSuite) SubTest(name string, test func()) {
+	oldT := s.t
+	oldAssertions := s.Assertions
+	s.T().Run(name, func(t *testing.T) {
+		s.t = t
+		s.Assertions = require.New(t)
+		test()
+	})
+	s.t = oldT
+	s.Assertions = oldAssertions
+}
+
+func (s *AbstractTestSuite) SubTestSuite(testingSuite suite.TestingSuite) {
+	suite.Run(s.t, testingSuite)
 }
